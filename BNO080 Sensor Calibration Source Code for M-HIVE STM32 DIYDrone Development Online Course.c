e3be2eccb5a98dff206e86adeb5225b69a7493a4
ChrisWonyeobPark <lbiith@naver.com>
2019-12-19 16:42:17 +0900

    Add files via upload
    BNO080 calibration source code for online course.
    https://www.inflearn.com/course/STM32CubelDE-STM32F4%EB%93%9C%EB%A1%A0-%EA%B0%9C%EB%B0%9C#

---
+++ BNO080 Sensor Calibration Source Code for M-HIVE STM32 DIYDrone Development Online Course.c
@@ -0,0 +1,155 @@
+/*
+  Using the BNO080 IMU
+  By: Nathan Seidle
+  SparkFun Electronics
+  Date: December 21st, 2017
+  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+
+  Feel like supporting our work? Buy a board from SparkFun!
+  https://www.sparkfun.com/products/14586
+
+  This example shows how to calibrate the sensor. See document 1000-4044.
+
+  It takes about 1ms at 400kHz I2C to read a record from the sensor, but we are polling the sensor continually
+  between updates from the sensor. Use the interrupt pin on the BNO080 breakout to avoid polling.
+
+  Hardware Connections:
+  Attach the Qwiic Shield to your Arduino/Photon/ESP32 or other
+  Plug the sensor onto the shield
+  Serial.print it out at 9600 baud to serial monitor.
+*/
+
+/**
+
+ * BNO080 Sensor Calibration Source Code for M-HIVE STM32 DIYDrone Development Online Course.txt
+ * @author ChrisP @ M-HIVE
+ * 
+ * This library source code has been modified for M-HIVE STM32 DIYDrone Development Online Course.
+ * The original example created by Nathan Seidle @ SparkFun Electronics.
+ *
+ * Development environment specifics:
+ * STM32CubeIDE 1.1.0
+ * STM32CubeF4 FW V1.24.1
+ * BNO080.c and BNO080.h library Rev. 1.0 (https://github.com/ChrisWonyeobPark/BNO080-STM32F4-SPI-LL-Driver/)
+ *
+ * Modified by ChrisP(Wonyeob Park) @ M-HIVE Embedded Academy, Dec, 2019
+ * Rev. 1.0
+ *
+ * https://github.com/ChrisWonyeobPark/
+ * https://cafe.naver.com/mhiveacademy
+ * https://www.inflearn.com/course/STM32CubelDE-STM32F4%EB%93%9C%EB%A1%A0-%EA%B0%9C%EB%B0%9C#
+ * 
+ * Special thanks to Nathan Seidle @ SparkFun Electronics, the original creator of the BNO080 arduino library.
+*/
+
+void BNO080_Calibration(void)
+{
+	RESET_LOW();
+	HAL_Delay(200);
+	RESET_HIGH();
+	HAL_Delay(300); //Reset BNO080
+
+	//BNO080/BNO085 Configuration
+	//Enable dynamic calibration for accelerometer, gyroscope, and magnetometer
+	//Enable Game Rotation Vector output
+	//Enable Magnetic Field output
+	BNO080_calibrateAll();
+	BNO080_enableGameRotationVector(10000);
+	BNO080_enableMagnetometer(10000);
+
+	printf("BNO080_Calibration Start\n");
+
+	//while loop for calibration procedure
+	//Iterates until iBus.SwC is mid point (1500)
+	//Calibration procedure should be done while this loop is in iteration.
+	while(iBus.SwC == 1500)
+	{
+		if(BNO080_dataAvailable() == 1)
+		{
+			//Observing the status bit of the magnetic field output
+			float x = BNO080_getMagX();
+			float y = BNO080_getMagY();
+			float z = BNO080_getMagZ();
+			unsigned char accuracy = BNO080_getMagAccuracy();
+
+			float quatI = BNO080_getQuatI();
+			float quatJ = BNO080_getQuatJ();
+			float quatK = BNO080_getQuatK();
+			float quatReal = BNO080_getQuatReal();
+			unsigned char sensorAccuracy = BNO080_getQuatAccuracy();
+
+			printf("%f,%f,%f,", x, y, z);
+			if (accuracy == 0) printf("Unreliable\t");
+			else if (accuracy == 1) printf("Low\t");
+			else if (accuracy == 2) printf("Medium\t");
+			else if (accuracy == 3) printf("High\t");
+
+			printf("\t%f,%f,%f,%f,", quatI, quatI, quatI, quatReal);
+			if (sensorAccuracy == 0) printf("Unreliable\t");
+			else if (sensorAccuracy == 1) printf("Low\t");
+			else if (sensorAccuracy == 2) printf("Medium\t");
+			else if (sensorAccuracy == 3) printf("High\n");
+
+			//Turn the LED and buzzer on when both accuracy and sensorAccuracy is high
+			if(accuracy == 3 && sensorAccuracy == 3)
+			{
+				LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2);
+				TIM3->PSC = 65000;
+				LL_TIM_CC_EnableChannel(TIM3, LL_TIM_CHANNEL_CH4);
+			}
+			else
+			{
+				LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2);
+				LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
+			}
+		}
+
+		Is_iBus_Received(); //Refreshes iBus Data for iBus.SwC
+		HAL_Delay(100);
+	}
+
+	//Ends the loop when iBus.SwC is not mid point
+	//Turn the LED and buzzer off
+	LL_TIM_CC_DisableChannel(TIM3, LL_TIM_CHANNEL_CH4);
+	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2);
+
+	//Saves the current dynamic calibration data (DCD) to memory
+	//Sends command to get the latest calibration status
+	BNO080_saveCalibration();
+	BNO080_requestCalibrationStatus();
+
+	//Wait for calibration response, timeout if no response
+	int counter = 100;
+	while(1)
+	{
+		if(--counter == 0) break;
+		if(BNO080_dataAvailable())
+		{
+			//The IMU can report many different things. We must wait
+			//for the ME Calibration Response Status byte to go to zero
+			if(BNO080_calibrationComplete() == 1)
+			{
+				printf("\nCalibration data successfully stored\n");
+				HAL_Delay(1000);
+				break;
+			}
+		}
+		HAL_Delay(10);
+	}
+	if(counter == 0)
+	{
+		printf("\nCalibration data failed to store. Please try again.\n");
+	}
+
+	RESET_LOW();
+	HAL_Delay(200);
+	RESET_HIGH();
+	HAL_Delay(300); //Resets BNO080 to disable Game Rotation Vector and Magnetometer
+
+	//Enables Rotation Vector
+	BNO080_enableRotationVector(2500);
+
+	//BNO080_endCalibration(); //Turns off all calibration
+	//In general, calibration should be left on at all times. The BNO080
+	//auto-calibrates and auto-records cal data roughly every 5 minutes
+}
